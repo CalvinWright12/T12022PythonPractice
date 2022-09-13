4d288ef8ce78e4384459625c1c06096d776a1b08
CalvinWright12 <826618@mystma.org>
2022-09-13 09:40:58 -0500

    3 Functions

---
+++ Main.py
@@ -0,0 +1,10 @@
+def nameYo():
+    print("Calvin")
+def topFive():
+    print("1. Bloodborne 2. EldenRing 3. DarkSoulsRem. 4. DemonsSouls 5. DarkSouls2/3")  
+def gambleWhiteYo():
+    print ("Well, my name is Skyler White, yo! My husband is Walter White, yo! Uh-huh, he told me everything.") 
+
+nameYo()
+topFive()
+gambleWhiteYo()             
